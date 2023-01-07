07442baf9bbe56400de3a9fe1374882b7f8decbc
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-01-07 03:00:57 +0300

    Updates

---
+++ demo3.483f7392.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".shutter");e.forEach(((t,o)=>{setTimeout((()=>{t.classList.add("show"),o===e.length-1&&setTimeout((()=>{e.forEach((e=>e.parentNode.removeChild(e)))}),450)}),300*(.4*o+1))}));
+//# sourceMappingURL=demo3.483f7392.js.map
